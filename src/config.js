feed6bb919d65784b23a15ce0e76b146fdf117fd
Zehra539 <zehra_al@skillup.online>
2024-01-18 02:37:39 -0500

    message

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://zehraal-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://zehraal-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+//export const API_URL = window.location.hostname === "localhost" ? "https://zehraal-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://zehraal-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+export const API_URL = window.location.origin
 console.log(
     "API_URL :",
     API_URL
