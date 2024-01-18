9d022710eea541e1389976882505ff7d638e77a9
Zehra539 <zehra_al@skillup.online>
2024-01-18 00:26:33 -0500

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import './App.css'; 
+import Login from './Components/Login/Login';
+import Navbar from './Components/Nav_bar/Navbar';
+import { Landing_Page } from './Components/Landing_Page/Landing_Page';
+import Sign_up from './Components/Sign_up/Sign_up';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import DoctorBook from './Components/DoctorCard/DoctorBook';
+import Notification from './Components/Notification/Notification';
+import ReviewForm from './Components/ReviewForm/ReviewForm';
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import ProfileForm from './Components/ProfileCard/ProfileForm';
+import ReportsLayout from './Components/ReportsLayout/ReportsLayout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+ 
+
+ 
+
+function App() { 
+
+  return ( 
+
+<BrowserRouter> 
+
+<Navbar/> 
+<Notification>
+
+<Routes> 
+
+<Route path='/' element={<Landing_Page/>}/>
+
+  <Route path='/login' element={<Login/>}/> 
+
+  <Route path='/signup' element={<Sign_up/>}/> 
+  <Route path='/finddoctor' element={<FindDoctorSearch/>}/>
+  <Route path="/search/doctors" element={<DoctorBook/>} />
+  <Route path='/review' element={<ReviewForm/>}/>
+  <Route path='/instant-consultation' element={<InstantConsultation/>}/>
+  <Route path='/profile' element={<ProfileForm/>} />
+  <Route path='/reports' element={<ReportsLayout/>} />
+  {/* <Route path='/login' element={<Login/>}/> */} 
+
+
+
+ 
+
+</Routes> 
+</Notification>
+
+</BrowserRouter> 
+
+ 
+
+   
+
+  ); 
+
+} 
+
+ 
+
+export default App; 
+      
--- src/Components/Landing_Page/Landing_Page.js
+++ src/Components/Landing_Page/Landing_Page.js
@@ -1,40 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./LandingPage.css";
-import Layout from './components/Landing_Page/Landing_page'};
+import React from 'react'
+import "./LandingPage.css"
 
-const Landing_Page = () => {
+export const Landing_Page = () => {
   return (
-    <section className="hero-section">
-      <div>
-        <div data-aos="fade-up" className="flex-hero">
+    <div><section class="hero-section">
+    <div>
+      <div data-aos="fade-up" class="flex-hero">
+          
+          <h1>
+            Your Health<br/>
+            <span class="text-gradient">
+              
+              Our Responsibility
+            </span>
+          </h1>
+            <div class="blob-cont">
+                <div class="blue blob"></div>
+            </div>
+            <div class="blob-cont">
+                <div class="blue1 blob"></div>
+            </div>
+          <h4>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem! 
+          </h4>
+          <a href="#services">
+            <button class="button">Get Started</button>
+          </a>
             
-            <h1>
-              Your Health<br/>
-
-              <span className="text-gradient">
-                
-                Our Responsibility
-              </span>
-            </h1>
-              <div class="blob-cont">
-                  <div class="blue blob"></div>
-              </div>
-              <div class="blob-cont">
-                  <div class="blue1 blob"></div>
-              </div>
-            <h4>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
-            </h4>
-            <a href="#services">
-              <button class="button">Get Started</button>
-            </a>
-        </div>
-
       </div>
-    </section>
-    <Route path="/" element={<Landing_Page/>}/>
-  );
-};
 
-export default Landing_Page;
+    </div>
+  </section></div>
+  )
+}
